2d8e3c89f71727294d0f793d602a3b046b96a069
M3wory <91751294+M3wory@users.noreply.github.com>
2021-10-02 09:23:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+if (typeof window !== 'undefined') {
+  if (LocalStorage.getItem('theme') == 'dark') {
+      theme.href = "css/dark-theme.css";
+  } else{
+      theme.href = "css/light-theme.css";
+      }
+}
 function checkbo() {
 var switchMode;
 switchMode=document.getElementById("modeCheck");
@@ -10,11 +17,4 @@ var theme = document.getElementById("theme");
 		theme.href = "css/light-theme.css";
 		localStorage.setItem('theme', 'light');
 	}
-}
-if (typeof window !== 'undefined') {
-  if (LocalStorage.getItem('theme') == 'dark') {
-      theme.href = "css/dark-theme.css";
-  } else{
-      theme.href = "css/light-theme.css";
-      }
 }
